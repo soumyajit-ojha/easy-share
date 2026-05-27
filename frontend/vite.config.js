e2a9e9cf226cefce1ffd10c5efe9460c36e582f1
soumyajit-ojha <soumyajitojhag2@gmail.com>
2026-05-27 11:49:06 +0530

    main branch cleanup

--- frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-// new line
-import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
